d3566335134ed1334c809133be8ff6dacc5fd80d
VZServer <vocaliczoo@gmail.com>
2024-04-27 13:43:06 +0800

    better ping cmd

--- src/commands/ping/index.js
+++ src/commands/ping/index.js
@@ -1,9 +1,18 @@
 import { SlashCommandBuilder } from "discord.js";
+import { useAppStore } from "@/store/app";
+const appStore = useAppStore();
+const client = appStore.client;
 
 export const command = new SlashCommandBuilder()
   .setName("ping")
   .setDescription("看看延遲!");
 
 export const action = async (ctx) => {
-  await ctx.reply("碰!");
+  const msg = await ctx.reply({
+    content: "正在計算延遲...!",
+    fetchReply: true,
+  });
+  const ping = msg.createdTimestamp - ctx.createdTimestamp;
+
+  ctx.editReply(`機器人延遲 : ${ping} ms\nAPI延遲 : ${client.ws.ping}`);
 };
